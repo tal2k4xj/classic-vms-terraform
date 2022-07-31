b4c9d08a7044dd62ba4666ac9c2f0d9699c17374
Tal Neeman <tal.neeman4489@gmail.com>
2022-07-31 22:37:49 +0300

    first commit

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "ibm_compute_vm_instance" "terraform-bulk-vms" {
   datacenter           = "dal09"
   network_speed        = 100
   hourly_billing       = true
-  private_network_only = true
+  private_network_only = false
   cores                = 1
   memory               = 1024
   disks                = [25]
